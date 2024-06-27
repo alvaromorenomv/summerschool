dcf946d6a5b512ed5bea02d2b221ff3f42899431
Alvaro Moreno <alvaro.moreno@bsc.es>
2024-06-27 14:06:46 +0200

    MPI user defined communicators exercise

--- mpi/communicator/skeleton.cpp
+++ mpi/communicator/skeleton.cpp
@@ -36,6 +36,13 @@ int main(int argc, char *argv[])
      *       use a single collective communication call
      *       (and maybe prepare some parameters for the call)
      */
+    if (rank < 2) {
+        color = 1;
+    } else {
+        color = 2;
+    }
+    MPI_Comm_split(MPI_COMM_WORLD, color, rank, &sub_comm);
+    MPI_Reduce(&sendbuf[0],&recvbuf[0],8,MPI_INT,MPI_SUM,0,sub_comm);
 
     /* Print data that was received */
     print_buffers(recvbuf);
