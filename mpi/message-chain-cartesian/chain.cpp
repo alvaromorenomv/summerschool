2b669314f8bb99e235a6adf31931d69dd629e2c0
Alvaro Moreno <alvaro.moreno@bsc.es>
2024-06-27 19:04:18 +0200

    MPI cartesian message chain exercise

---
+++ mpi/message-chain-cartesian/chain.cpp
@@ -0,0 +1,82 @@
+#include <cstdio>
+#include <vector>
+#include <mpi.h>
+
+void print_ordered(double t);
+
+int main(int argc, char *argv[])
+{
+    int i, myid, ntasks;
+    constexpr int size = 10000000;
+    std::vector<int> message(size);
+    std::vector<int> receiveBuffer(size);
+    MPI_Status status;
+    int neighbors[2] = {0}; /* Neighbors in 2D grid */
+    int period[1] = {1};
+    MPI_Comm comm2d;
+
+    double t0, t1;
+
+    int source, destination, tag;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+    // Initialize buffers
+    for (i = 0; i < size; i++) {
+        message[i] = myid;
+        receiveBuffer[i] = -1;
+    }
+
+    // TODO: Set source and destination ranks
+    // TODO: Treat boundaries with MPI_PROC_NULL
+    source = (myid == 0) ? MPI_PROC_NULL : myid-1;
+    destination = (myid == ntasks-1) ? MPI_PROC_NULL : myid+1;
+    MPI_Cart_create(MPI_COMM_WORLD,1,&ntasks,period,1,&comm2d);
+    MPI_Cart_shift(comm2d,0,1, &source, &destination);
+    int cart_id;
+    MPI_Comm_rank(comm2d, &cart_id);
+
+    // Start measuring the time spent in communication
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+
+    // TODO: Send messages
+     MPI_Sendrecv(message.data(), message.size(), MPI_INT, destination, (cart_id + 1)%ntasks,
+                 receiveBuffer.data(), receiveBuffer.size(), MPI_INT, source, cart_id,
+                 comm2d, &status);
+
+    printf("Sender: %d. Sent elements: %d. Tag: %d. Receiver: %d\n",
+           myid, size, (myid + 1)%ntasks, destination);
+
+    printf("Receiver: %d. first element %d.\n",
+           myid, receiveBuffer[0]);
+
+    // Finalize measuring the time and print it out
+    t1 = MPI_Wtime();
+    MPI_Barrier(MPI_COMM_WORLD);
+    fflush(stdout);
+
+    print_ordered(t1 - t0);
+
+    MPI_Finalize();
+    return 0;
+}
+
+void print_ordered(double t)
+{
+    int i, rank, ntasks;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+
+    if (rank == 0) {
+        printf("Time elapsed in rank %2d: %6.3f\n", rank, t);
+        for (i = 1; i < ntasks; i++) {
+            MPI_Recv(&t, 1, MPI_DOUBLE, i, 11, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Time elapsed in rank %2d: %6.3f\n", i, t);
+        }
+    } else {
+        MPI_Send(&t, 1, MPI_DOUBLE, 0, 11, MPI_COMM_WORLD);
+    }
+}
