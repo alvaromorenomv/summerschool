ebde13ab9ea2f984b998a7c8522548bab374f6c9
Alvaro Moreno <alvaro.moreno@bsc.es>
2024-06-27 10:42:06 +0200

    MPI collective reductions exercicse

--- mpi/heat-equation/cpp/io.cpp
+++ mpi/heat-equation/cpp/io.cpp
@@ -81,16 +81,9 @@ void read_field(Field& field, std::string filename,
         for (int i = 0; i < field.nx; i++)
             for (int j = 0; j < field.ny; j++)
                 inner(i, j) = full(i, j);
-
-        // Send data to others
-        for (int p=1; p < parallel.size; p++) {
-            MPI_Send(full.data(p * field.nx, 0), field.nx * field.ny,
-                     MPI_DOUBLE, p, 22, MPI_COMM_WORLD);
-        }
-    } else {
-        MPI_Recv(inner.data(), field.nx * field.ny,
-                 MPI_DOUBLE, 0, 22, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
+    } 
+    MPI_Scatter(full.data(), field.nx * field.ny_full, MPI_DOUBLE, inner.data(),
+    field.nx * field.ny_full,MPI_DOUBLE,0,MPI_COMM_WORLD);
 
     file.close();
 
