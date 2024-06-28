f892a13c09f15c14b5378aee64ce25ad06ed71eb
Alvaro Moreno <alvaro.moreno@bsc.es>
2024-06-28 12:02:15 +0200

    HDF5 Write-read exercise

--- parallel-io/hdf5-write-read/hdf5.c
+++ parallel-io/hdf5-write-read/hdf5.c
@@ -56,19 +56,33 @@ void h5_writer(int my_id, int *localvector, int localsize)
     hid_t plist_id, dset_id, filespace, memspace, file_id;
     hsize_t dims, counts, offsets;
 
+    dims = DATASIZE;
+    counts = localsize;
+    offsets = my_id * localsize;
     /* Create the handle for parallel file access property list
        and create a new file */
+    plist_id = H5Pcreate(H5P_FILE_ACCESS);
+    H5Pset_fapl_mpio(plist_id, MPI_COMM_WORLD, MPI_INFO_NULL);
+    file_id = H5Fcreate("sos_bobo.h5", H5F_ACC_TRUNC, H5P_DEFAULT, plist_id);
 
     /* Create the dataset */
-
+    filespace = H5Screate_simple(1, &dims, NULL);
+    dset_id = H5Dcreate(file_id, "sos_bobo", H5T_NATIVE_INT, filespace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
     /* Select a hyperslab of the file dataspace */
-
+    H5Sselect_hyperslab(filespace, H5S_SELECT_SET, &offsets, NULL, &counts, NULL);
     /* Now we can write our local data to the correct position in the
        dataset. Here we use collective write, but independent writes are
        also possible. */
+    memspace = H5Screate_simple(1, &counts, NULL);
+    H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, H5P_DEFAULT, localvector);
 
     /* Close all opened HDF5 handles */
-}
+    H5Dclose(dset_id);
+    H5Sclose(filespace);
+    H5Sclose(memspace);
+    H5Fclose(file_id);
+    H5Pclose(plist_id);
+}   
 
 
 void h5_reader(int my_id, int *localvector, int localsize)
@@ -79,14 +93,33 @@ void h5_reader(int my_id, int *localvector, int localsize)
 
     /* Create the handle for parallel file access property list
        and open a file for reading */
+    dims = DATASIZE;
+    counts = localsize;
+    offsets = my_id * localsize;
 
+    plist_id = H5Pcreate(H5P_FILE_ACCESS);
+    H5Pset_fapl_mpio(plist_id, MPI_COMM_WORLD, MPI_INFO_NULL);
+    file_id = H5Fopen("sos_bobo.h5", H5F_ACC_RDONLY, plist_id);
+    H5Pclose(plist_id);
     /* Open the dataset and get the filespace id */
-
+    dset_id = H5Dopen(file_id, "sos_bobo", H5P_DEFAULT);
+    filespace = H5Dget_space(dset_id);
     /* Select a hyperslab of the file dataspace */
-
+    H5Sselect_hyperslab(filespace, H5S_SELECT_SET, &offsets, NULL, &counts, NULL);
     /* Now we can read our local data from the correct position in the
        dataset. Here we use collective read but independent reads are
        also possible. */
+    memspace = H5Screate_simple(1, &counts, NULL);
+    plist_id = H5Pcreate(H5P_DATASET_XFER);
+    H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
+
+    status = H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace,
+                      plist_id, localvector);
 
     /* Close all opened HDF5 handles */
+    H5Dclose(dset_id);
+    H5Pclose(plist_id);
+    H5Sclose(filespace);
+    H5Sclose(memspace);
+    H5Fclose(file_id);
 }
