a74ffddf7eb17de4bdbb60d3aa8ec9f961d44deb
Alvaro Moreno <alvaro.moreno@bsc.es>
2024-06-28 14:20:39 +0200

    Reduction exercise OpenMP

---
+++ hybrid/reduction/skeleton.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <omp.h>
+
+#define NX 102400
+
+int main(void)
+{
+    long vecA[NX];
+    long sum, sumex;
+    int i;
+
+    /* Initialization of the vectors */
+    for (i = 0; i < NX; i++) {
+        vecA[i] = (long) i + 1;
+    }
+
+    sumex = (long) NX * (NX + 1) / ((long) 2);
+    printf("Arithmetic sum formula (exact):                  %ld\n",
+           sumex);
+
+    sum = 0.0;
+    /* TODO: Parallelize computation */
+    #pragma omp parallel 
+    {
+        long psum = 0.0;
+        #pragma omp for
+        for (i = 0; i < NX; i++) {
+            psum += vecA[i];
+        }
+        #pragma omp critical 
+            sum += psum;
+        
+    }
+    printf("Sum: %ld\n", sum);
+
+    return 0;
+}
